d1742548abb3c45dfc0107d9cb68126fd09ab8a3
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-05-25 21:14:54 +0200

    Adding bullets

--- src/components/Bullets/Bullets.jsx
+++ src/components/Bullets/Bullets.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useContext, useRef } from 'react'
+import { StateContext } from '../../store/store'
 import styles from './Bullets.module.css'
+import anime from 'animejs'
+import * as actionTypes from '../../store/actionTypes'
 
 const Bullets = (props) => {
 
-    return (
-        <div className={`${styles.body} ${styles[props.type]}`}></div>
-    )
+    const element = useRef()
+    const store = useContext(StateContext)
+
+    useEffect(() => {
+        anime({
+            targets: element.current,
+            translateY: -store.state.game.arenaHeight,
+            duration: 1000,
+            easing: 'linear',
+            change() {
+                // const rect = element.current.getBoundingClientRect()
+            },
+            complete(anim) {
+                if (anim.completed) {
+                    const payload = {
+                        index: props.index,
+                        bullet: {
+                            type: props.type,
+                            position: {
+                                x: 0,
+                                y: 0
+                            }
+                        }
+                    }
+                    store.dispatch({ type: actionTypes.BULLET_MOVE, payload })
+                }
+            }
+        })
+    }, [])
+
+    const style = {
+        left: props.x + 'px',
+        top: props.y + 'px'
+    }
+
+    let bullet = null
+
+    if (props.y > 0) {
+        bullet = <div ref={element} style={style} className={`${styles.body} ${styles[props.type]}`}></div>
+    }
+
+    return bullet
 }
 
 export default Bullets 
--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -63,7 +63,14 @@ const Game = (props) => {
         }
 
         const handleShoot = () => {
-            console.log('Test2')
+            const payload = {
+                type: store.state.player.gun,
+                position: {
+                    x: store.state.game.playerPosition.x + 28,
+                    y: store.state.game.playerPosition.y - 28
+                }
+            }
+            store.dispatch({ type: actionTypes.BULLET_CREATE, payload })
         }
 
         window.addEventListener('keydown', handleMove)
@@ -78,10 +85,18 @@ const Game = (props) => {
 
     return (
         <React.Fragment>
-            <Bullets type='laser' />
+            {store.state.game.playerBullets.map((bullet, index) =>
+                <Bullets
+                    key={index}
+                    index={index}
+                    type={bullet.type}
+                    x={bullet.position.x}
+                    y={bullet.position.y}
+                />
+            )}
             <Battleship
                 color={store.state.player.selectedShip}
-                animationTime={starting ? 900 : 100}
+                animationTime={starting ? 900 : 0}
                 flyingLeft={flyingLeft}
                 flyingRight={flyingRight}
                 x={store.state.game.playerPosition.x === 0 ? null : store.state.game.playerPosition.x}
