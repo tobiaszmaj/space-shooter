e8052118f1eb01029cc3068a215bdb89ff4813a2
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-06-13 22:46:47 +0200

    Updates

---
+++ precache-manifest.db4c0c65e16122eabf55ac665753744c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b93aaf9487a1a837ec5",
+    "url": "/space-shooter/static/css/main.384e3ced.chunk.css"
+  },
+  {
+    "revision": "2b93aaf9487a1a837ec5",
+    "url": "/space-shooter/static/js/main.2b93aaf9.chunk.js"
+  },
+  {
+    "revision": "91a8dd262415bcd99b66",
+    "url": "/space-shooter/static/js/runtime~main.91a8dd26.js"
+  },
+  {
+    "revision": "96eda266c64967980ffa",
+    "url": "/space-shooter/static/js/2.96eda266.chunk.js"
+  },
+  {
+    "revision": "96e4396edf3284fd21bad89dbd5d378d",
+    "url": "/space-shooter/static/media/battleship_blue.96e4396e.png"
+  },
+  {
+    "revision": "ae65df042714c009a92f91aca240ca1d",
+    "url": "/space-shooter/static/media/battleship_red.ae65df04.png"
+  },
+  {
+    "revision": "f2458ac906c0026bdd03748d3e9a4d5d",
+    "url": "/space-shooter/static/media/platform.f2458ac9.png"
+  },
+  {
+    "revision": "8d66e0ed13e40978334ab27aeac03e01",
+    "url": "/space-shooter/static/media/bg.8d66e0ed.jpg"
+  },
+  {
+    "revision": "79a5fadf54a3399f259cbe8ac435efd0",
+    "url": "/space-shooter/index.html"
+  }
+];
